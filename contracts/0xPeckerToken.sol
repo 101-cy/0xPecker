23a2ade1ba341c86e837970ff1137db0fe40c11a
Marios Isaakidis <prometheas@autistici.org>
2022-01-29 04:07:36 +0200

    NFT mint, Check overlap with memberLocks
    * revert() returns error messages
    * isMemberLocked function set to view
    * Comments
    * DAO.101.cy Aragon link

--- contracts/0xPeckerToken.sol
+++ contracts/0xPeckerToken.sol
@@ -1,13 +1,13 @@
-// contracts/LeaseToken.sol
+// contracts/0xPeckerToken.sol
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.10;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-/// @title HexPeckerToken: leasing token for accessing base.101.cy
+/// @title HexPeckerToken: sublet token for accessing base.101.cy
 /// @custom:security-contact marios@101.cy
 contract HexPeckerToken is ERC721 {
 
@@ -16,6 +16,7 @@ contract HexPeckerToken is ERC721 {
     Counters.Counter private _tokenIdCounter;
 
     // Address of the DAO.101.cy membership token contract on Rinkeby
+    // https://client.aragon.org/#/rinkeby101cy.aragonid.eth
     address constant DAOTokenContract = address(0xE4786c3289F3F3fFac1C75C92C0c03B56fA4f25F);
 
     // Interface for DAO membership tokens -- Aragon Chainlink tokens
@@ -31,45 +32,44 @@ contract HexPeckerToken is ERC721 {
 
     constructor() ERC721("0xPecker", "0xPKR") {}
 
-    function isMemberLocked(address member, uint256 since, uint256 until) public returns (bool) {
+    function isMemberLocked(address member, uint256 since, uint256 until) public view returns (bool) {
         lockPeriod[] memory memberLocks = lockedMembers[member];
-        if (memberLocks.length == 0) {
-            // There is no entry for this member
-            return false;
-        }
-
-        //TODO: Iterate memberLocks
-
-        if (memberLock[until] < block.timestamp) {
-            // An older entry for this member that has expired, delete
-            return false;
-        }
         
-        //TODO: Check overlaps
+        // Iterate memberLocks
+        for (uint i = 0; i < memberLocks.length; i++) {
+            if (memberLocks[i].until < block.timestamp) {
+                // An older entry for this member that has expired, delete
+                //TODO: memberLocks.remove(i);
+                continue;
+            }
+            if (since <= memberLocks[i].until && memberLocks[i].since <= until) {
+                return true;
+            }
+        }
 
-        return true;
+        return false;
     }
 
-    function getTokenById(uint256 tokendId) external {
+    function getTokenStatus(uint256 tokendId) external pure returns (address member, uint256 since, uint256 until) {
+        //TODO: What happens to 0xPecker tokens after members burn/transfer their DAO token?
     }
 
     // Mint 0xPecker sublet token for DAOTokenId, since, until, to recipient
     // since and until are UNIX epochs
     function safeMint(uint256 since, uint256 until, address to) public {
+        if (DAOTokenInterface.balanceOf(msg.sender) < 1) revert(); // Sender does not have a DAO membership token
 
-        if (since > until) revert(); // The since timestamp needs to be before the until timestamp
+        if (since > until) revert("The since timestamp needs to be before the until timestamp");
 
-        if (until < block.timestamp) revert(); // Given until timestamp is in the past
+        if (until < block.timestamp) revert("Given until timestamp is in the past");
 
-        if (isMemberLocked(msg.sender, since, until)) revert(); // An existing period overlaps with given since/until
+        if (isMemberLocked(msg.sender, since, until)) revert("An existing period overlaps with given since/until");
 
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
 
         lockedMembers[msg.sender].push(lockPeriod(tokenId, since, until)); // Lock member for that period
-
-        //TODO: What happens to 0xPecker tokens after members burns/transfers their DAO token?
     }
 
 
